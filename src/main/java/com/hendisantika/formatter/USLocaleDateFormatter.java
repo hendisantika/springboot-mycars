cc8d1ac4df4e0d9acccaf0892bdd67fdafef3a08
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-03 07:17:40 +0700

    #12 - Add USLocaleDateFormatter

---
+++ src/main/java/com/hendisantika/formatter/USLocaleDateFormatter.java
@@ -0,0 +1,42 @@
+package com.hendisantika.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mycars
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/02/22
+ * Time: 07.17
+ * To change this template use File | Settings | File Templates.
+ */
+public class USLocaleDateFormatter implements Formatter<LocalDate> {
+
+    public static final String US_PATTERN = "MM/dd/yyyy";
+    public static final String NORMAL_PATTERN = "dd-MM-yyyy";
+
+    public static String getPattern(Locale locale) {
+        return isUnitedStates(locale) ? US_PATTERN : NORMAL_PATTERN;
+    }
+
+    private static boolean isUnitedStates(Locale locale) {
+        return Locale.US.getCountry().equals(locale.getCountry());
+    }
+
+    @Override
+    public LocalDate parse(String text, Locale locale) throws ParseException {
+        return LocalDate.parse(text, DateTimeFormatter.ofPattern(getPattern(locale)));
+    }
+
+    @Override
+    public String print(LocalDate object, Locale locale) {
+        return DateTimeFormatter.ofPattern(getPattern(locale)).format(object);
+    }
+}
