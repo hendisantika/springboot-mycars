da6a8853ddeb80daef1389f01b72e252148b7b53
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-03 07:10:24 +0700

    #7 - Add WebConfiguration

---
+++ src/main/java/com/hendisantika/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mycars
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/02/22
+ * Time: 07.09
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+}
