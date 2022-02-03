269a7d5dc5d4d9c4ed50251445020760bc6a9aa1
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-03 07:33:24 +0700

    #27 - Add UserDetailService

---
+++ src/main/java/com/hendisantika/service/UserDetailService.java
@@ -0,0 +1,16 @@
+package com.hendisantika.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mycars
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/02/22
+ * Time: 07.32
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserDetailService extends UserDetailsService {
+}
