101f0676b9056350b925203aa292c1d283e2f190
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-03 07:29:28 +0700

    #21 - Add User Entity

---
+++ src/main/java/com/hendisantika/entity/User.java
@@ -0,0 +1,107 @@
+package com.hendisantika.entity;
+
+import com.hendisantika.dto.ProfileForm;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mycars
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/02/22
+ * Time: 07.27
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "USER")
+@Data
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "LOGIN", nullable = false, unique = true)
+    private String login;
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+    @Column(name = "LAST_NAME", nullable = false)
+    private String lastName;
+    private String email;
+    private String phone;
+    @Lob
+    private byte[] profileImage;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "OWNER_ID")
+    private Set<Car> cars;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false)
+    private Set<Authority> authorities;
+
+    public User(String login, String password, String firstName, String lastName) {
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(String login, String password, String firstName, String lastName, String email, String phone) {
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public void updateProfileFromProfileForm(ProfileForm profileForm) {
+        login = profileForm.getLogin();
+        password = profileForm.getPassword();
+        firstName = profileForm.getFirstName();
+        lastName = profileForm.getLastName();
+        phone = profileForm.getPhone();
+        email = profileForm.getEmail();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
