e44fe5f752bd35f640f0c674afb60e1116770f4f
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-03 07:20:08 +0700

    #14 - Add NoteDTO

---
+++ src/main/java/com/hendisantika/dto/NoteDTO.java
@@ -0,0 +1,41 @@
+package com.hendisantika.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mycars
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/02/22
+ * Time: 07.19
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoteDTO {
+    private Long id;
+    @NotNull
+    private LocalDate date;
+    @NotNull
+    @NotEmpty
+    private String title;
+    @NotNull
+    @NotEmpty
+    private String description;
+    @DecimalMin(value = "0")
+    @NotNull
+    private Long partsCost;
+    @DecimalMin(value = "0")
+    @NotNull
+    private Long serviceCost;
+}
