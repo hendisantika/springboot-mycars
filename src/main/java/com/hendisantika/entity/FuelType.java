7beadfa4a34d0d14e8a38c71fbf2569334c0c6ad
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-03 07:24:09 +0700

    #17 - Add FuelType Enum

---
+++ src/main/java/com/hendisantika/entity/FuelType.java
@@ -0,0 +1,15 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mycars
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/02/22
+ * Time: 07.23
+ * To change this template use File | Settings | File Templates.
+ */
+public enum FuelType {
+    DIESEL, PETROL, HYBRID, LPG
+}
