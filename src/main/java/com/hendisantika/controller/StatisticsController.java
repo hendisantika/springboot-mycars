4dd1d5fa320f8fcf36a00ef2d3ccd9bc2445764b
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-04 17:48:24 +0700

    #70 - Add showCarStatisticsPage in StatisticsController

--- src/main/java/com/hendisantika/controller/StatisticsController.java
+++ src/main/java/com/hendisantika/controller/StatisticsController.java
@@ -5,7 +5,11 @@ import com.hendisantika.repository.CarRepository;
 import com.hendisantika.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by IntelliJ IDEA.
@@ -34,5 +38,32 @@ public class StatisticsController {
     public StatisticsController(CarRepository carRepository, NoteRepository noteRepository) {
         this.carRepository = carRepository;
         this.noteRepository = noteRepository;
+    }
+
+    /**
+     * Response for GET request, method passes calculated statistics to model
+     *
+     * @param id    current car id
+     * @param cost  fuel cost from user
+     * @param model view model
+     * @return
+     */
+    @GetMapping(value = "/{id}/statistics")
+    public String showCarStatisticsPage(@PathVariable(name = "id") String id,
+                                        @RequestParam(name = "cost", defaultValue = "1") Float cost,
+                                        Model model) {
+
+        car = carRepository.findById(Long.valueOf(id)).get();
+        calculateStatisticsForOneCar(car, cost);
+
+        model.addAttribute("id", id);
+        model.addAttribute("carPrice", car.getCarDetails().getPrice());
+        model.addAttribute("serviceCost", serviceCost);
+        model.addAttribute("partsCost", partsCost);
+        model.addAttribute("totalCost", totalCost);
+        model.addAttribute("distance", car.getMileage());
+        model.addAttribute("distanceCost", distanceCost);
+
+        return STATISTICS_PAGE_NAME;
     }
 }
