8d2c56ffe2c8d35ae62f988fb70bf3e6624323f3
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-04 17:46:12 +0700

    #67 - Add submitRegistrationForm in RegisterController

--- src/main/java/com/hendisantika/controller/RegisterController.java
+++ src/main/java/com/hendisantika/controller/RegisterController.java
@@ -7,8 +7,12 @@ import com.hendisantika.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -48,4 +52,34 @@ public class RegisterController {
         model.addAttribute("profileForm", new ProfileForm());
         return REGISTRATION_PAGE_NAME;
     }
+
+    /**
+     * Called when user submits registration form
+     *
+     * @param profileForm   new user form
+     * @param bindingResult form's validation result
+     * @param model         View model
+     * @return registration page name
+     */
+    @PostMapping
+    public String submitRegistrationForm(@Valid ProfileForm profileForm,
+                                         BindingResult bindingResult,
+                                         Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return REGISTRATION_PAGE_NAME;
+        }
+
+        if (userRepository.findByLogin(profileForm.getLogin()) != null) {
+            model.addAttribute("error", true);
+            return REGISTRATION_PAGE_NAME;
+        } else {
+            model.addAttribute("error", false);
+        }
+
+        registerUserInDb(profileForm);
+
+        return LOGIN_PAGE_NAME;
+    }
+
 }
