c9d5b599f378397a1fe54c3647dcf208d127bd17
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-04 17:30:21 +0700

    #46 - Add submitAddCarHistoryNote in HistoryController

--- src/main/java/com/hendisantika/controller/HistoryController.java
+++ src/main/java/com/hendisantika/controller/HistoryController.java
@@ -7,9 +7,13 @@ import com.hendisantika.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -56,5 +60,27 @@ class HistoryController {
         model.addAttribute("id", id);
 
         return HISTORY_PAGE_NAME;
+    }
+
+    /**
+     * Response for POST,if new note from form is submitted
+     *
+     * @param id            car id
+     * @param form          note from form on the page
+     * @param bindingResult validation result
+     * @return name of the page with car history
+     */
+    @PostMapping(value = "/{id}/history", params = {"add"})
+    public String submitAddCarHistoryNote(@PathVariable(name = "id") String id,
+                                          @Valid NoteDTO form,
+                                          BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return HISTORY_PAGE_NAME;
+        }
+
+        saveNoteInDb(form);
+
+        return String.format(REDIRECT_TO_HISTORY_PAGE_URL, id);
     }
 }
