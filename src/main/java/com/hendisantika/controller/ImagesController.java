2e9f0869f9d59fe457bfaaf7742ffbd782cd9dc9
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-04 17:36:41 +0700

    #54 - Add writeCarImage in ImagesController

--- src/main/java/com/hendisantika/controller/ImagesController.java
+++ src/main/java/com/hendisantika/controller/ImagesController.java
@@ -1,11 +1,13 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Car;
 import com.hendisantika.entity.User;
 import com.hendisantika.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -42,6 +44,26 @@ public class ImagesController {
             User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
             response.getOutputStream().write(user.getProfileImage());
+            response.setContentType("image/jpg");
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * Called from car details page, return car's image
+     *
+     * @param id       car id
+     * @param request  request
+     * @param response response
+     */
+    @RequestMapping("/image/cars/car")
+    public void writeCarImage(@RequestParam(name = "id") String id,
+                              HttpServletRequest request,
+                              HttpServletResponse response) {
+        try {
+            Car car = carRepository.findById(Long.parseLong(id)).get();
+
+            response.getOutputStream().write(car.getCarImage());
             response.setContentType("image/jpg");
         } catch (Exception e) {
         }
