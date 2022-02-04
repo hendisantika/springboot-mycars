d5c506d7a4ef920ebacf6ba3ff4603fd168f460f
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-04 17:37:23 +0700

    #55 - Add LoginController

---
+++ src/main/java/com/hendisantika/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.hendisantika.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mycars
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/02/22
+ * Time: 17.37
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    private static final String LOGIN_PAGE_NAME = "login";
+}
