44f11593effbba2727e67238bee497b4c60292de
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-04 17:37:39 +0700

    #56 - Add showLoginPage in LoginController

--- src/main/java/com/hendisantika/controller/LoginController.java
+++ src/main/java/com/hendisantika/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.hendisantika.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -18,4 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     private static final String LOGIN_PAGE_NAME = "login";
+
+    @GetMapping
+    public String showLoginPage() {
+        return LOGIN_PAGE_NAME;
+    }
 }
