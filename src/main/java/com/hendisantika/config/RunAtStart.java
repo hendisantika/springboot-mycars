126d859994d914d97737284b046eb62987cedd82
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-27 19:47:50 +0700

    #90 - Modify RunAtStart

--- src/main/java/com/hendisantika/config/RunAtStart.java
+++ src/main/java/com/hendisantika/config/RunAtStart.java
@@ -1,5 +1,11 @@
 package com.hendisantika.config;
 
+import com.hendisantika.repository.AuthorityRepository;
+import com.hendisantika.repository.CarRepository;
+import com.hendisantika.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-mycars
@@ -10,5 +16,16 @@ package com.hendisantika.config;
  * Time: 07.08
  * To change this template use File | Settings | File Templates.
  */
+@Component
 public class RunAtStart {
+    private final UserRepository userRepository;
+    private final CarRepository carRepository;
+    private final AuthorityRepository authorityRepository;
+
+    @Autowired
+    public RunAtStart(UserRepository userRepository, CarRepository carRepository, AuthorityRepository authorityRepository) {
+        this.userRepository = userRepository;
+        this.carRepository = carRepository;
+        this.authorityRepository = authorityRepository;
+    }
 }
